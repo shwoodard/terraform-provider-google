e5bf170a87de2600ddbe2900d9b47e782df85329
Sam Woodard <sam.h.woodard@gmail.com>
2022-08-26 07:45:19 -0700

    dry off tests

--- google/compute_operation_test.go
+++ google/compute_operation_test.go
@@ -1,20 +1,88 @@
 package google
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
 	"google.golang.org/api/compute/v1"
 )
 
-var omitAlways = []string{
-	"LocalizedMessage2",
-	"Help1Link2 Description",
-	"https://help1.com/link2",
-	"Help2Link1 Description",
-	"https://help2.com/link1",
-	"Help2Link2 Description",
-	"https://help2.com/link2",
+const (
+	topLevelMsg             = "Top-level message."
+	localizedMsgTmpl        = "LocalizedMessage%d message"
+	helpLinkDescriptionTmpl = "Help%dLink%d Description"
+	helpLinkUrlTmpl         = "https://help%d.com/link%d"
+)
+
+var locales = []string{"en-US", "es-US", "es-ES", "es-MX", "de-DE"}
+
+func buildOperationError(numLocalizedMsg int, numHelpWithLinks []int) compute.OperationError {
+	opError := &compute.OperationErrorErrors{Message: topLevelMsg}
+	opErrorErrors := []*compute.OperationErrorErrors{opError}
+
+	for n := 1; n <= numLocalizedMsg; n++ {
+		opError.ErrorDetails = append(opError.ErrorDetails,
+			&compute.OperationErrorErrorsErrorDetails{
+				LocalizedMessage: &compute.LocalizedMessage{
+					Locale:  locales[n-1%len(locales)],
+					Message: formatLocalizedMsg(n),
+				},
+			})
+	}
+
+	for i := 0; i < len(numHelpWithLinks); i++ {
+		errorDetail := &compute.OperationErrorErrorsErrorDetails{
+			Help: &compute.Help{},
+		}
+
+		for nLinks := 1; nLinks <= numHelpWithLinks[i]; nLinks++ {
+			desc, url := formatLink(i+1, nLinks)
+			errorDetail.Help.Links = append(errorDetail.Help.Links, &compute.HelpLink{
+				Description: desc,
+				Url:         url,
+			})
+		}
+
+		opError.ErrorDetails = append(opError.ErrorDetails, errorDetail)
+	}
+
+	return compute.OperationError{Errors: opErrorErrors}
+
+}
+
+func omitAlways(numLocalizedMsg int, numHelpWithLinks []int) []string {
+	var omits []string
+
+	for n := 2; n <= numLocalizedMsg; n++ {
+		omits = append(omits, fmt.Sprintf("LocalizedMessage%d", n))
+	}
+
+	for i := 0; i < len(numHelpWithLinks); i++ {
+		for j := maxLinks(i); j < numHelpWithLinks[i]; j++ {
+			desc, url := formatLink(i+1, j+1)
+			omits = append(omits, desc, url)
+		}
+	}
+
+	return omits
+
+}
+
+func maxLinks(helpIndex int) int {
+	if helpIndex == 0 {
+		return 1
+	}
+
+	return 0
+}
+
+func formatLocalizedMsg(localizedMsgNum int) string {
+	return fmt.Sprintf(localizedMsgTmpl, localizedMsgNum)
+}
+
+func formatLink(helpNum, linkNum int) (string, string) {
+	return fmt.Sprintf(helpLinkDescriptionTmpl, helpNum, linkNum), fmt.Sprintf(helpLinkUrlTmpl, helpNum, linkNum)
 }
 
 func TestComputeOperationError_Error(t *testing.T) {
@@ -25,252 +93,90 @@ func TestComputeOperationError_Error(t *testing.T) {
 		expectOmits    []string
 	}{
 		{
-			name: "MessageOnly",
-			input: compute.OperationError{
-				Errors: []*compute.OperationErrorErrors{
-					&compute.OperationErrorErrors{
-						Message: "Top-level message.",
-					},
-				},
-			},
+			name:  "MessageOnly",
+			input: buildOperationError(0, []int{}),
 			expectContains: []string{
 				"Top-level",
 			},
-			expectOmits: append(omitAlways, []string{
+			expectOmits: append(omitAlways(0, []int{}), []string{
 				"LocalizedMessage1",
 				"Help1Link1 Description",
 				"https://help1.com/link1",
 			}...),
 		},
 		{
-			name: "WithLocalizedMessageAndNoHelp",
-			input: compute.OperationError{
-				Errors: []*compute.OperationErrorErrors{
-					&compute.OperationErrorErrors{
-						Message: "Top-level message.",
-						ErrorDetails: []*compute.OperationErrorErrorsErrorDetails{
-							&compute.OperationErrorErrorsErrorDetails{
-								LocalizedMessage: &compute.LocalizedMessage{
-									Locale:  "en-US",
-									Message: "LocalizedMessage1 message",
-								},
-							},
-						},
-					},
-				},
-			},
+			name:  "WithLocalizedMessageAndNoHelp",
+			input: buildOperationError(1, []int{}),
 			expectContains: []string{
 				"Top-level",
 				"LocalizedMessage1",
 			},
-			expectOmits: append(omitAlways, []string{
+			expectOmits: append(omitAlways(1, []int{}), []string{
 				"Help1Link1 Description",
 				"https://help1.com/link1",
 			}...),
 		},
 		{
-			name: "WithLocalizedMessageAndHelp",
-			input: compute.OperationError{
-				Errors: []*compute.OperationErrorErrors{
-					&compute.OperationErrorErrors{
-						Message: "Top-level message.",
-						ErrorDetails: []*compute.OperationErrorErrorsErrorDetails{
-							&compute.OperationErrorErrorsErrorDetails{
-								LocalizedMessage: &compute.LocalizedMessage{
-									Locale:  "en-US",
-									Message: "LocalizedMessage1 message",
-								},
-							},
-							&compute.OperationErrorErrorsErrorDetails{
-								Help: &compute.Help{
-									Links: []*compute.HelpLink{
-										&compute.HelpLink{
-											Description: "Help1Link1 Description",
-											Url:         "https://help1.com/link1",
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
+			name:  "WithLocalizedMessageAndHelp",
+			input: buildOperationError(1, []int{1}),
 			expectContains: []string{
 				"Top-level",
 				"LocalizedMessage1",
 				"Help1Link1 Description",
 				"https://help1.com/link1",
 			},
-			expectOmits: append(omitAlways, []string{}...),
+			expectOmits: append(omitAlways(1, []int{1}), []string{}...),
 		},
 		{
-			name: "WithNoLocalizedMessageAndHelp",
-			input: compute.OperationError{
-				Errors: []*compute.OperationErrorErrors{
-					&compute.OperationErrorErrors{
-						Message: "Top-level message.",
-						ErrorDetails: []*compute.OperationErrorErrorsErrorDetails{
-							&compute.OperationErrorErrorsErrorDetails{
-								Help: &compute.Help{
-									Links: []*compute.HelpLink{
-										&compute.HelpLink{
-											Description: "Help1Link1 Description",
-											Url:         "https://help1.com/link1",
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
+			name:  "WithNoLocalizedMessageAndHelp",
+			input: buildOperationError(0, []int{1}),
 			expectContains: []string{
 				"Top-level",
 				"Help1Link1 Description",
 				"https://help1.com/link1",
 			},
-			expectOmits: append(omitAlways, []string{
+			expectOmits: append(omitAlways(0, []int{1}), []string{
 				"LocalizedMessage1",
 			}...),
 		},
 		{
-			name: "WithLocalizedMessageAndHelpWithTwoLinks",
-			input: compute.OperationError{
-				Errors: []*compute.OperationErrorErrors{
-					&compute.OperationErrorErrors{
-						Message: "Top-level message.",
-						ErrorDetails: []*compute.OperationErrorErrorsErrorDetails{
-							&compute.OperationErrorErrorsErrorDetails{
-								LocalizedMessage: &compute.LocalizedMessage{
-									Locale:  "en-US",
-									Message: "LocalizedMessage1 message",
-								},
-							},
-							&compute.OperationErrorErrorsErrorDetails{
-								Help: &compute.Help{
-									Links: []*compute.HelpLink{
-										&compute.HelpLink{
-											Description: "Help1Link1 Description",
-											Url:         "https://help1.com/link1",
-										},
-										&compute.HelpLink{
-											Description: "Help1Link2 Description",
-											Url:         "https://help1.com/link2",
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
+			name:  "WithLocalizedMessageAndHelpWithTwoLinks",
+			input: buildOperationError(1, []int{2}),
 			expectContains: []string{
 				"Top-level",
 				"LocalizedMessage1",
 				"Help1Link1 Description",
 				"https://help1.com/link1",
 			},
-			expectOmits: append(omitAlways, []string{}...),
+			expectOmits: append(omitAlways(1, []int{2}), []string{}...),
 		},
 		// The case below should never happen because the server should just send multiple links
 		// but the protobuf defition would allow it, so testing anyway.
 		{
-			name: "WithLocalizedMessageAndTwoHelpsWithTwoLinks",
-			input: compute.OperationError{
-				Errors: []*compute.OperationErrorErrors{
-					&compute.OperationErrorErrors{
-						Message: "Top-level message.",
-						ErrorDetails: []*compute.OperationErrorErrorsErrorDetails{
-							&compute.OperationErrorErrorsErrorDetails{
-								LocalizedMessage: &compute.LocalizedMessage{
-									Locale:  "en-US",
-									Message: "LocalizedMessage1 message",
-								},
-							},
-							&compute.OperationErrorErrorsErrorDetails{
-								Help: &compute.Help{
-									Links: []*compute.HelpLink{
-										&compute.HelpLink{
-											Description: "Help1Link1 Description",
-											Url:         "https://help1.com/link1",
-										},
-										&compute.HelpLink{
-											Description: "Help1Link2 Description",
-											Url:         "https://help1.com/link2",
-										},
-									},
-								},
-							},
-							&compute.OperationErrorErrorsErrorDetails{
-								Help: &compute.Help{
-									Links: []*compute.HelpLink{
-										&compute.HelpLink{
-											Description: "Help2Link1 Description",
-											Url:         "https://help2.com/link1",
-										},
-										&compute.HelpLink{
-											Description: "Help2Link2 Description",
-											Url:         "https://help2.com/link2",
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
+			name:  "WithLocalizedMessageAndTwoHelpsWithTwoLinks",
+			input: buildOperationError(1, []int{2, 2}),
 			expectContains: []string{
 				"Top-level",
 				"LocalizedMessage1",
 				"Help1Link1 Description",
 				"https://help1.com/link1",
 			},
-			expectOmits: append(omitAlways, []string{}...),
+			expectOmits: append(omitAlways(1, []int{2, 2}), []string{}...),
 		},
 		// This should never happen because the server should never respond with the messages for
 		// two locales at once, but should rather take the locale as input to the API and serve
 		// the appropriate message for that locale. However, the protobuf defition would allow it,
 		// so we'll test for it. The second message in the list would be ignored.
 		{
-			name: "WithTwoLocalizedMessageAndHelp",
-			input: compute.OperationError{
-				Errors: []*compute.OperationErrorErrors{
-					&compute.OperationErrorErrors{
-						Message: "Top-level message.",
-						ErrorDetails: []*compute.OperationErrorErrorsErrorDetails{
-							&compute.OperationErrorErrorsErrorDetails{
-								LocalizedMessage: &compute.LocalizedMessage{
-									Locale:  "en-US",
-									Message: "LocalizedMessage1 message",
-								},
-							},
-							&compute.OperationErrorErrorsErrorDetails{
-								LocalizedMessage: &compute.LocalizedMessage{
-									Locale:  "en-ES",
-									Message: "LocalizedMessage2 message",
-								},
-							},
-							&compute.OperationErrorErrorsErrorDetails{
-								Help: &compute.Help{
-									Links: []*compute.HelpLink{
-										&compute.HelpLink{
-											Description: "Help1Link1 Description",
-											Url:         "https://help1.com/link1",
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
+			name:  "WithTwoLocalizedMessageAndHelp",
+			input: buildOperationError(2, []int{1}),
 			expectContains: []string{
 				"Top-level",
 				"LocalizedMessage1",
 				"Help1Link1 Description",
 				"https://help1.com/link1",
 			},
-			expectOmits: append(omitAlways, []string{}...),
+			expectOmits: append(omitAlways(2, []int{1}), []string{}...),
 		},
 	}
 
